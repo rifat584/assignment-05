366e6512e7c63bcc99a7f9eeb1fe8d67eececb8b
Rifat Mahmud <rifat584>
2025-08-28 00:39:11 +0600

    Added Responsivness v1

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+/*
+
+const mainElement= document.getElementById('history');
+const history= document.createElement('div');
+
+history.innerHTML = `
+ <div>
+      <div style="width:full; background-color:#fafafa; padding:16px; border-radius:16px">
+        <h4 style="font-weight:500; font-size:20px">জাতীয় জরুরি সেবা</h4>
+        <p>999</p>
+      </div>
+      
+    </div>
+`
+mainElement.appendChild(history);
+
+*/
