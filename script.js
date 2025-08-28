9e641a3d6606d0a865ff25ef169b8f1604d9390d
Rifat Mahmud <rifat584>
2025-08-28 07:25:01 +0600

    added history functionality

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
-
 // Heart Counter
-const heart = document.getElementById('heart-count')
-let newNum= 0; 
+const heart = document.getElementById("heart-count");
+let newNum = 0;
 
-const findHearts= document.getElementsByClassName('fa-heart')
-console.log(find)
+const findHearts = document.getElementsByClassName("fa-heart");
+console.log(find);
 
-for (const find of findHearts){
-  find.addEventListener('click', function(){
-    newNum++
-    heart.innerHTML= newNum
-  })
+for (const find of findHearts) {
+  find.addEventListener("click", function () {
+    newNum++;
+    heart.innerHTML = newNum;
+  });
 }
 
 // Credit Reducing
-const coin = document.getElementById('coin-count');
+const coin = document.getElementById("coin-count");
 let coinCount = 100;
 
-const findCalls = document.getElementsByClassName('btn-primary')
-for(const btn of findCalls){
-  btn.addEventListener('click', function(){
-    if(coinCount<20){
-      alert ("You don't have enough coins")
-      
-    } else{
-      
+const findCalls = document.getElementsByClassName("btn-primary");
+for (const btn of findCalls) {
+  btn.addEventListener("click", function (e) {
+    const serviceName =
+      e.target.parentNode.parentNode.childNodes[3].childNodes[3].innerText;
+
+    const serviceNameBengali =
+      e.target.parentNode.parentNode.childNodes[3].childNodes[1].innerText;
+    const serviceNum =
+      e.target.parentNode.parentNode.childNodes[3].childNodes[5].innerText;
+
+    const getTime = new Date();
+    const timeStr = getTime.toLocaleTimeString();
+
+    if (coinCount < 20) {
+      alert("You don't have enough coins");
+    } else {
       coinCount = coinCount - 20;
-      coin.innerHTML= coinCount;
+      coin.innerHTML = coinCount;
+      // Adding to History
+      const mainElement = document.getElementById("history");
+      const history = document.createElement("div");
+
+      history.innerHTML = `
+        <div>
+        <h4> ${serviceNameBengali}</h4>
+        <p>${serviceNum}</p>
+        </div>
+        <p> ${timeStr} </p>
+`;
+      history.classList.add(
+        "serviceContainerStyle",
+        "primary-font",
+        "historyContainer"
+      );
+
+      const serviceNameBengaliStyle = history.childNodes[1].childNodes[1];
+      serviceNameBengaliStyle.classList.add("serviceNameStyle");
+
+      mainElement.appendChild(history);
+      // Alert with service name & Number
+      alert(`Calling ${serviceName} ${serviceNum}`);
     }
-  })
+  });
 }
-
-
-
-
-/*
-
-const mainElement= document.getElementById('history');
-const history= document.createElement('div');
-
-history.innerHTML = `
- <div>
-      <div style="width:full; background-color:#fafafa; padding:16px; border-radius:16px">
-        <h4 style="font-weight:500; font-size:20px">জাতীয় জরুরি সেবা</h4>
-        <p>999</p>
-      </div>
-      
-    </div>
-`
-mainElement.appendChild(history);
-
-*/
